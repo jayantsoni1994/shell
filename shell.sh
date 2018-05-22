98d2d0f0faa94e269596e025ee20c7578a1ff8b5
jayantsoni1994 <jayantsoni1994.dl@gmail.com>
2018-05-22 09:07:28 +0000

    shell

---
+++ shell.sh
@@ -0,0 +1,8 @@
+sudo yum install -y git
+sudo yum install -y maven
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl status httpd
+echo "hello world My name is jayant soni"
+cd /var/www/html/
+sudo git pull /var/www/html/
